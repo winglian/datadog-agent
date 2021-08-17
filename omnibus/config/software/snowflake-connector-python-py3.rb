005ecea27b668ed5abd9a754a813a800231f681b
Florian Veaux <florian.veaux@datadoghq.com>
2021-08-17 13:16:16 +0200

    Update snowflake-connector on py3

--- omnibus/config/software/snowflake-connector-python-py3.rb
+++ omnibus/config/software/snowflake-connector-python-py3.rb
@@ -2,7 +2,7 @@ name "snowflake-connector-python-py3"
 
 dependency "pip3"
 
-default_version "2.1.3"
+default_version "2.5.1"
 
 source :url => "https://github.com/snowflakedb/snowflake-connector-python/archive/v#{version}.tar.gz",
        :sha256 => "855ffb93a09c3cd994dab8af7c87a46038bbba103928c5948a0edcd2500f4e1a",
