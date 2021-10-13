160b4b3e206f547d84b7ce3243d65ed2988cec96
Dustin J. Mitchell <dustin@v.igoro.us>
2021-10-13 19:31:47 +0000

    no documentation
    This commit is tagged with a high version number so that it is displayed
    at https://pkg.go.dev/github.com/DataDog/datadog-agent.

---
+++ docs.go
@@ -0,0 +1,19 @@
+// The present repository contains the source code of the Datadog Agent version 7
+// and version 6. Please refer to the [Agent user documentation](docs/agent) for
+// information about differences between Agent v5, Agent v6 and Agent v7.
+// Additionally, we provide a list of prepackaged binaries for an easy install
+// process [here](https://app.datadoghq.com/account/settings#agent)
+//
+// **Note:** the source code of Datadog Agent v5 is located in the
+// [dd-agent](https://github.com/DataDog/dd-agent) repository.
+//
+// ## Documentation
+//
+// This repository is not intended to be linked to other go packages, as it does
+// not provide any API-compatibility guarantees.  As a result, there are no
+// publicly-hosted generated godocs for this repository.  Several sub-packages,
+// such as `pkg/util/logs`, are available for linking.
+//
+// The general documentation of the project, including instructions for installation
+// and development, is located under [the docs directory](docs) of the present repo.
+package main
