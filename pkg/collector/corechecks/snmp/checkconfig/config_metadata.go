90b7824bf09662ae4ca96ece1753e7dead03f519
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-11-09 11:00:59 +0100

    refactor LegacyMetadataConfig

--- pkg/collector/corechecks/snmp/checkconfig/config_metadata.go
+++ pkg/collector/corechecks/snmp/checkconfig/config_metadata.go
@@ -15,10 +15,6 @@ var LegacyMetadataConfig = MetadataConfig{
 				OID:  "1.3.6.1.2.1.1.5.0",
 				Name: "sysName",
 			},
-			"serial_number": {
-				OID:  "1.3.6.1.4.1.3375.2.1.3.3.3.0",
-				Name: "sysGeneralChassisSerialNum",
-			},
 			"sys_object_id": {
 				OID:  "1.3.6.1.2.1.1.2.0",
 				Name: "sysObjectID",
@@ -27,29 +23,29 @@ var LegacyMetadataConfig = MetadataConfig{
 	},
 	"interface": {
 		Fields: map[string]SymbolConfig{
-			"admin_status": {
-				OID:  "1.3.6.1.2.1.2.2.1.7",
-				Name: "ifAdminStatus",
-			},
-			"alias": {
-				OID:  "1.3.6.1.2.1.31.1.1.1.18",
-				Name: "ifAlias",
+			"name": {
+				OID:  "1.3.6.1.2.1.31.1.1.1.1",
+				Name: "ifName",
 			},
 			"description": {
 				OID:  "1.3.6.1.2.1.2.2.1.2",
 				Name: "ifDescr",
 			},
-			"mac_address": {
-				OID:  "1.3.6.1.2.1.2.2.1.6",
-				Name: "ifPhysAddress",
-			},
-			"name": {
-				OID:  "1.3.6.1.2.1.31.1.1.1.1",
-				Name: "ifName",
+			"admin_status": {
+				OID:  "1.3.6.1.2.1.2.2.1.7",
+				Name: "ifAdminStatus",
 			},
 			"oper_status": {
 				OID:  "1.3.6.1.2.1.2.2.1.8",
 				Name: "ifOperStatus",
+			},
+			"alias": {
+				OID:  "1.3.6.1.2.1.31.1.1.1.18",
+				Name: "ifAlias",
+			},
+			"mac_address": {
+				OID:  "1.3.6.1.2.1.2.2.1.6",
+				Name: "ifPhysAddress",
 			},
 		},
 		IDTags: MetricTagConfigList{
