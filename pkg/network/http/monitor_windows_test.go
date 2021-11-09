3b35aa4ffd18219373eb0479f7de5738ae82500c
Isabelle Sauve <isabelle.sauve@datadoghq.com>
2021-11-09 19:47:49 +0000

    Reduce wait time

--- pkg/network/http/monitor_windows_test.go
+++ pkg/network/http/monitor_windows_test.go
@@ -60,7 +60,7 @@ func testHTTPMonitor(t *testing.T, targetAddr, serverAddr string, numReqs int) {
 	}
 
 	// Ensure all captured transactions get sent to user-space
-	time.Sleep(15 * time.Second)
+	time.Sleep(5 * time.Second)
 	monitor.di.flushPendingTransactions()
 	time.Sleep(5 * time.Second)
 
