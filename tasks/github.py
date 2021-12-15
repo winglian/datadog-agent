7c02207c3ae83d4a4d42dd40715b25077ec07720
Albert Vaca Cintora <albert.vaca@datadoghq.com>
2021-12-15 17:32:00 +0100

    Test

--- tasks/github.py
+++ tasks/github.py
@@ -41,7 +41,7 @@ def trigger_macos_test(
 
     env = load_release_versions(ctx, release_version)
     github_action_ref = env["MACOS_BUILD_VERSION"]
-    github_action_ref = "72536387d7e0f6cd1970e4d0f846c2c3a3968567"
+    github_action_ref = "albertvaka/add-test-workflow"
 
     run_id = trigger_macos_workflow(
         workflow="test.yaml",
