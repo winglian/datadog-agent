3eba05dec1ea98a751c38ecb3dcbae964687f609
truthbk <truthbk@gmail.com>
2022-01-19 12:17:58 +0100

    [jmx] standalone: fix no-jmx builds

--- cmd/agent/app/standalone/jmx_nojmx.go
+++ cmd/agent/app/standalone/jmx_nojmx.go
@@ -3,23 +3,27 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
+//go:build !jmx
 // +build !jmx
 
 package standalone
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // ExecJMXCommandConsole is not supported when the 'jmx' build tag isn't included
-func ExecJMXCommandConsole(command string, selectedChecks []string, logLevel string) error {
+func ExecJMXCommandConsole(command string, selectedChecks []string, logLevel string, discoveryRetryInterval time.Duration, discoveryTimeout time.Duration, discoveryMinInstances uint) error {
 	return fmt.Errorf("not supported: the Agent is compiled without the 'jmx' build tag")
 }
 
 // ExecJmxListWithMetricsJSON is not supported when the 'jmx' build tag isn't included
-func ExecJmxListWithMetricsJSON(selectedChecks []string, logLevel string) error {
+func ExecJmxListWithMetricsJSON(selectedChecks []string, logLevel string, discoveryRetryInterval time.Duration, discoveryTimeout time.Duration, discoveryMinInstances uint) error {
 	return fmt.Errorf("not supported: the Agent is compiled without the 'jmx' build tag")
 }
 
 // ExecJmxListWithRateMetricsJSON is not supported when the 'jmx' build tag isn't included
-func ExecJmxListWithRateMetricsJSON(selectedChecks []string, logLevel string) error {
+func ExecJmxListWithRateMetricsJSON(selectedChecks []string, logLevel string, discoveryRetryInterval time.Duration, discoveryTimeout time.Duration, discoveryMinInstances uint) error {
 	return fmt.Errorf("not supported: the Agent is compiled without the 'jmx' build tag")
 }
