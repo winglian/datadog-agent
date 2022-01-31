022ca44d838d1b417b955ae52160643b3132d0fa
Olivier G <olivier.gaca@datadoghq.com>
2022-01-31 10:05:04 +0100

    remove zlib from json_payload_builder

--- pkg/serializer/stream/json_payload_builder_nozlib.go
+++ pkg/serializer/stream/json_payload_builder_nozlib.go
@@ -3,39 +3,36 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-present Datadog, Inc.
 
-//go:build !zlib
-// +build !zlib
-
 package stream
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/DataDog/datadog-agent/pkg/forwarder"
-	"github.com/DataDog/datadog-agent/pkg/serializer/marshaler"
-)
+// 	"github.com/DataDog/datadog-agent/pkg/forwarder"
+// 	"github.com/DataDog/datadog-agent/pkg/serializer/marshaler"
+// )
 
-// OnErrItemTooBigPolicy defines the behavior when OnErrItemTooBig occurs.
-type OnErrItemTooBigPolicy int
+// // OnErrItemTooBigPolicy defines the behavior when OnErrItemTooBig occurs.
+// type OnErrItemTooBigPolicy int
 
-const (
-	// DropItemOnErrItemTooBig when founding an ErrItemTooBig, skips the error and continue
-	DropItemOnErrItemTooBig OnErrItemTooBigPolicy = iota
+// const (
+// 	// DropItemOnErrItemTooBig when founding an ErrItemTooBig, skips the error and continue
+// 	DropItemOnErrItemTooBig OnErrItemTooBigPolicy = iota
 
-	// FailOnErrItemTooBig when founding an ErrItemTooBig, returns the error and stop
-	FailOnErrItemTooBig
-)
+// 	// FailOnErrItemTooBig when founding an ErrItemTooBig, returns the error and stop
+// 	FailOnErrItemTooBig
+// )
 
-// JSONPayloadBuilder is not implemented when zlib is not available.
-type JSONPayloadBuilder struct {
-}
+// // JSONPayloadBuilder is not implemented when zlib is not available.
+// type JSONPayloadBuilder struct {
+// }
 
-// NewJSONPayloadBuilder is not implemented when zlib is not available.
-func NewJSONPayloadBuilder(shareAndLockBuffers bool) *JSONPayloadBuilder {
-	return nil
-}
+// // NewJSONPayloadBuilder is not implemented when zlib is not available.
+// func NewJSONPayloadBuilder(shareAndLockBuffers bool) *JSONPayloadBuilder {
+// 	return nil
+// }
 
-// BuildWithOnErrItemTooBigPolicy is not implemented when zlib is not available.
-func (b *JSONPayloadBuilder) BuildWithOnErrItemTooBigPolicy(marshaler.IterableStreamJSONMarshaler, OnErrItemTooBigPolicy) (forwarder.Payloads, error) {
-	return nil, fmt.Errorf("not implemented")
-}
+// // BuildWithOnErrItemTooBigPolicy is not implemented when zlib is not available.
+// func (b *JSONPayloadBuilder) BuildWithOnErrItemTooBigPolicy(marshaler.IterableStreamJSONMarshaler, OnErrItemTooBigPolicy) (forwarder.Payloads, error) {
+// 	return nil, fmt.Errorf("not implemented")
+// }
