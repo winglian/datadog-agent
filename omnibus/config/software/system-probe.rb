b8cacf2ce4050f0de63b734fd8cf4dd7ed11e3e8
Isabelle Sauve <isabelle.sauve@datadoghq.com>
2022-02-02 17:19:23 -0500

    Include clang binary in agent package

--- omnibus/config/software/system-probe.rb
+++ omnibus/config/software/system-probe.rb
@@ -34,6 +34,7 @@ build do
     copy "#{ENV['SYSTEM_PROBE_BIN']}/conntrack.c", "#{install_dir}/embedded/share/system-probe/ebpf/runtime/"
     copy "#{ENV['SYSTEM_PROBE_BIN']}/oom-kill.c", "#{install_dir}/embedded/share/system-probe/ebpf/runtime/"
     copy "#{ENV['SYSTEM_PROBE_BIN']}/tcp-queue-length.c", "#{install_dir}/embedded/share/system-probe/ebpf/runtime/"
+    copy "#{ENV['SYSTEM_PROBE_BIN']}/clang", "#{install_dir}/embedded/bin/clang"
   end
 
   copy 'pkg/ebpf/c/COPYING', "#{install_dir}/embedded/share/system-probe/ebpf/"
