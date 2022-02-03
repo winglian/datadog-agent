1b1ba8810c1ab54cf3f825c156a14371d93f68c2
Isabelle Sauve <isabelle.sauve@datadoghq.com>
2022-02-03 02:30:17 -0500

    Add more logging + capture http compilation telemetry

--- pkg/ebpf/bytecode/runtime/runtime_compilation_helpers.go
+++ pkg/ebpf/bytecode/runtime/runtime_compilation_helpers.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/DataDog/datadog-agent/pkg/ebpf"
 	"github.com/DataDog/datadog-agent/pkg/ebpf/compiler"
+	"github.com/DataDog/datadog-agent/pkg/security/log"
 	"github.com/DataDog/datadog-agent/pkg/security/metrics"
 	"github.com/DataDog/datadog-agent/pkg/util/kernel"
 	"github.com/DataDog/datadog-agent/pkg/version"
@@ -174,6 +175,7 @@ func (rc *RuntimeCompiler) CompileObjectFile(config *ebpf.Config, cflags []strin
 			return nil, fmt.Errorf("failed to compile runtime version of %s: %s", inputFileName, err)
 		}
 		rc.telemetry.compilationResult = compilationSuccess
+		log.Infof("successfully compiled runtime version of %s", inputFileName)
 	} else {
 		rc.telemetry.compilationResult = compiledOutputFound
 	}
