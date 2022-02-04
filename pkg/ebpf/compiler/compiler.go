678a82506343037974f8a5580285533937ec15ce
Isabelle Sauve <isabelle.sauve@datadoghq.com>
2022-02-04 16:46:22 -0500

    Increase timeout

--- pkg/ebpf/compiler/compiler.go
+++ pkg/ebpf/compiler/compiler.go
@@ -75,7 +75,7 @@ func CompileToObjectFile(in io.Reader, outputFile string, cflags []string, heade
 
 	var clangOut, clangErr, llcOut, llcErr bytes.Buffer
 
-	clangCtx, clangCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	clangCtx, clangCancel := context.WithTimeout(context.Background(), 1*time.Minute)
 	defer clangCancel()
 
 	compileToBC := exec.CommandContext(clangCtx, clangBinPath, cflags...)
