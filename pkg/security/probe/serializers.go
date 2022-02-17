0ecef1d7769ff65b2989b3d3b8a460becb19f5b5
Gui774ume <guillaume.fournier@datadoghq.com>
2022-02-17 15:20:00 +0100

    select probes based on loaded modules

--- pkg/security/probe/serializers.go
+++ pkg/security/probe/serializers.go
@@ -646,7 +646,7 @@ func newDNSEventSerializer(d *model.DNSEvent) *DNSEventSerializer {
 	}
 }
 
-func newIPPortSerializer(c *model.IpPortContext) *IPPortSerializer {
+func newIPPortSerializer(c *model.IPPortContext) *IPPortSerializer {
 	return &IPPortSerializer{
 		IP:   c.IP.String(),
 		Port: c.Port,
