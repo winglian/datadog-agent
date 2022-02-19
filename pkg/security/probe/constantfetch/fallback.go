c5d7adcd812c7a95e739e3fe580c7ddb41207a2d
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-02-19 21:12:54 +0100

    [CWS] fix `pid_numbers_offset` on Ubuntu kernel 5.0

--- pkg/security/probe/constantfetch/fallback.go
+++ pkg/security/probe/constantfetch/fallback.go
@@ -372,7 +372,11 @@ func getPIDNumbersOffset(kv *kernel.Version) uint64 {
 	case kv.IsSLES15Kernel():
 		pidNumbersOffset = 80
 
-	case kv.IsInRangeCloseOpen(kernel.Kernel4_15, kernel.Kernel5_3):
+	case kv.IsInRangeCloseOpen(kernel.Kernel4_15, kernel.Kernel5_0):
+		pidNumbersOffset = 48
+	case kv.IsInRangeCloseOpen(kernel.Kernel5_0, kernel.Kernel5_1):
+		pidNumbersOffset = 56
+	case kv.IsInRangeCloseOpen(kernel.Kernel5_1, kernel.Kernel5_3):
 		pidNumbersOffset = 48
 	case kv.IsInRangeCloseOpen(kernel.Kernel5_3, kernel.Kernel5_7):
 		pidNumbersOffset = 80
