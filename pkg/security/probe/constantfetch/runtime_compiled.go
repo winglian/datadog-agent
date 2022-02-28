cda72137b2923c3f07a61d145f02d99b14741104
Sylvain Baubeau <lebauce@gmail.com>
2022-02-28 23:39:04 +0000

    Export ErrorSentinel from constant fetcher

--- pkg/security/probe/constantfetch/runtime_compiled.go
+++ pkg/security/probe/constantfetch/runtime_compiled.go
@@ -57,7 +57,7 @@ func (cf *RuntimeCompilationConstantFetcher) AppendSizeofRequest(id, typeName, h
 		Id:        id,
 		Operation: fmt.Sprintf("sizeof(%s)", typeName),
 	})
-	cf.result[id] = errorSentinel
+	cf.result[id] = ErrorSentinel
 }
 
 func (cf *RuntimeCompilationConstantFetcher) AppendOffsetofRequest(id, typeName, fieldName, headerName string) {
@@ -69,7 +69,7 @@ func (cf *RuntimeCompilationConstantFetcher) AppendOffsetofRequest(id, typeName,
 		Id:        id,
 		Operation: fmt.Sprintf("offsetof(%s, %s)", typeName, fieldName),
 	})
-	cf.result[id] = errorSentinel
+	cf.result[id] = ErrorSentinel
 }
 
 const runtimeCompilationTemplate = `
